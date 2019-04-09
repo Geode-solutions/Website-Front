efd3ac7b0aa1d078d9ab261c27d82e89620b47d7
Arnaud Botella <arnaud.botella@total.com>
2019-04-09 09:30:29 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,4 @@
-<template>
+s<template>
   <v-layout
     column
     justify-center
@@ -13,7 +13,7 @@
         <v-card-title class="headline">Welcome to the Geode-solutions website</v-card-title>
         <v-card-text class="text-xs-center">
           <p>This site is under construction.</p>
-          <p>Please come back soon...</p>
+          <p>Let's build it!</p>
         </v-card-text>
       </v-card>
     </v-flex>
