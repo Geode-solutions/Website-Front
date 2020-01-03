eea159eb93f986c5c0fbbe297587e955e8e2794d
BotellaA <arnaud.botella@geode-solutions.com>
2020-01-03 14:23:27 +0100

    add services page

--- pages/products.vue
+++ pages/products.vue
@@ -15,64 +15,11 @@
         and available on Windows, macOS and Linux.
       </p>
     </v-col>  
-    <v-container fluid style="background-color: white">
-      <v-row class="container mx-auto">
-        <product category="Modeling" :products="modelingProducts" />
-      </v-row>
-    </v-container>
-
-    <v-container>
-      <v-row>
-        <product category="Meshing" :products="meshingProducts" />
-      </v-row>
-    </v-container>
   </v-row>
 </template>
 
 <script>
-import Product from "@/components/Product"
-
 export default {
-  components: {
-    Product
-  },
-  data: () => ({
-    modelingProducts: [
-        {
-          images: [],
-          title: 'Modeling from surfaces',
-          text:
-            'Build your sealed model by inserting/removing surfaces incrementally, always valid at each step.'
-        },
-        {
-          images: [],
-          title: 'Modeling from data points',
-          text:
-            "Compute surfaces fitting your set of points and design your sealed model"
-        }
-      ],
-    meshingProducts: [
-        {
-          images: [],
-          title: 'Validating and repairing',
-          text:
-            'Checking mesh and/or model required features to design your simulation application'
-        },
-        {
-          images: [],
-          title: 'Surface model remeshing',
-          text:
-            "Robust triangulated surface model remesher, adapted to any geometric constraint."
-        },
-        {
-          images: [],
-          title: 'Volume model meshing',
-          text:
-            "Robust tetrahedral model mesher, adapted to any geometric constraint."
-        }
-      ]
-    }
-  ),
   head() {
     return {
       title: 'Geode-solutions products for geometric and geosciences models'
