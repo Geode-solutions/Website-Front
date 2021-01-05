d781a9713a4afd6fcda1bd9fd2ecf93167ecced1
Arnaud Botella <botella.arnaud@gmail.com>
2021-01-05 14:44:48 +0100

    wip

--- pages/credits.vue
+++ pages/credits.vue
@@ -7,7 +7,11 @@
         </h2>
 
         <h4>
-          All pictures are under Pixabay Licence (Free for commercial use, No attribution
+          All pictures in "Expertize" and "Software" pages are Copyright — Geode-solutions SAS. All rights reserved.
+        </h4>
+
+        <h4>
+          All other pictures are under Pixabay Licence (Free for commercial use, No attribution
           required)
         </h4>
       </v-col>
