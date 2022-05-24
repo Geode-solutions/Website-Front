75aa37b8d80bc39f2de64288005e78a27ae6cad5
Julien Champagnol <91873154+JulienChampagnol@users.noreply.github.com>
2022-05-24 09:41:49 +0200

    Feature/validitychecker (#44)
    * CrossSection & StructuralModel
    
    * Add RegularGrid2D & RegularGrid3D
    
    * Create netlify.toml
    
    * /test/tools/createbackend
    
    * test SITE_BRANCH
    
    * else test= ''
    
    * test context.next
    
    * console.log(this.$config)
    
    * console.log(this.$config)
    
    * test context."next"
    
    * SITE_BRANCH: process.env.SITE_BRANCH
    
    * [context."next".environment]
    
    * test: this.$config.SITE_BRANCH
    
    * test
    
    * if NODE_ENV = production
    
    * Feature/vuex (#39)
    
    * Change cards in file converter
    
    * feat(wip) : vuex
    
    * feat(wip) : vuex
    
    * vuex
    
    * feat(wip) : vuex
    
    * vuex
    
    * save
    
    * save
    
    * 2nd commit
    
    * feat(wip) : clean and refactor
    
    * watch feature
    
    * Update index.js
    
    * Delete vuex.js
    
    * Update nuxt.config.js
    
    * cleanup
    
    * axios setBaseURL
    
    * state.connexionLaunched
    
    * Update plugins/axios.js
    
    Co-authored-by: Arnaud Botella <arnaud.botella@geode-solutions.com>
    
    Co-authored-by: Arnaud Botella <arnaud.botella@geode-solutions.com>
    
    * if (this.state.connexionLaunched) { return }
    
    * SITE_BRANCH = "/test"
    
    * delete vuex actions from child components
    
    * keep-alive
    
    * PackagesVersions component
    
    * feat(wip): validty checker
    
    * UI control panel
    
    * cleanup and working
    
    * cleanup and working
    
    * PR comments changes
    
    * remove async from GetTestsResults
    
    Co-authored-by: Arnaud Botella <arnaud.botella@geode-solutions.com>

--- pages/tools/validitychecker.vue
+++ pages/tools/validitychecker.vue
@@ -1,25 +1,325 @@
 <template>
   <v-container>
-    <v-row justify="center">
-      <v-col cols="12">
-        <v-card class="card" elevation="2">
-          <v-card-title>Coming soon!</v-card-title>
-          <v-card-text>
-            This free tool is currently under development. Stay tuned!
-          </v-card-text>
-        </v-card>
+    <v-row class="flex-column">
+      <v-col>
+        <h1 class="text-h2 py-5" align="center">Validity checker</h1>
+        <v-col>
+          <v-row justify="space-around">
+            <v-col v-for="(item, i) in items" :key="i" cols="11" md="5">
+              <v-card
+                v-ripple
+                class="card"
+                hover
+                elevation="5"
+                :href="item.href"
+                target="_blank"
+                contain
+              >
+                <v-row justify="center" align="center">
+                  <v-col cols="auto">
+                    <v-icon size="128" class="justify-center">
+                      {{ item.icon }}
+                    </v-icon>
+                  </v-col>
+                </v-row>
+                <v-card-title
+                  primary-title
+                  class="justify-center text-h6"
+                  align="center"
+                >
+                  {{ item.title }}
+                </v-card-title>
+                <v-card-text class="justify-center text-body-1">
+                  {{ item.text }}
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-col>
+      <v-col v-if="!cloudRunning">
+        <cloud-loading />
+      </v-col>
+
+      <v-col v-else class="pb-5">
+        <v-stepper v-model="currentStep" class="stepper" vertical>
+          <v-stepper-step
+            :complete="currentStep > 1"
+            step="1"
+            @click="SetStep(1)"
+          >
+            <v-row align="center">
+              <v-col cols="auto"> Please select a file to check </v-col>
+              <v-col>
+                <v-chip v-if="files.length">
+                  {{ files[0].name }}
+                </v-chip>
+              </v-col>
+            </v-row>
+          </v-stepper-step>
+
+          <v-stepper-content step="1">
+            <v-file-input
+              chips
+              rounded
+              :multiple="multiple"
+              color="#3b3b3b"
+              :label="inputMessage"
+              :accept="acceptedExtensions"
+              :rules="inputRules"
+              show-size
+              :success="success"
+              @click:clear="objects = []"
+              @change="GetAllowedObjects"
+            />
+          </v-stepper-content>
+
+          <v-stepper-step
+            :complete="currentStep > 2"
+            step="2"
+            @click="SetStep(2)"
+          >
+            <v-row align="center">
+              <v-col cols="auto"> Confirm the data type </v-col>
+              <v-col>
+                <v-chip v-if="GeodeObject">
+                  {{ GeodeObject }}
+                </v-chip>
+              </v-col>
+            </v-row>
+          </v-stepper-step>
+
+          <v-stepper-content step="2">
+            <v-row v-if="objects.length">
+              <v-col>
+                <v-row justify="left">
+                  <v-col
+                    v-for="object in objects"
+                    :key="object"
+                    cols="2"
+                    md="2"
+                  >
+                    <v-tooltip bottom>
+                      <template #activator="{ on }">
+                        <v-card
+                          v-ripple
+                          class="card ma-2"
+                          hover
+                          elevation="5"
+                          v-on="on"
+                        >
+                          <v-img
+                            :src="
+                              require('@/assets/tools/' +
+                                GeodeObjects[object].image)
+                            "
+                            contain
+                            @click="SetGeodeObject(object)"
+                          />
+                        </v-card>
+                      </template>
+                      <span>{{ GeodeObjects[object].tooltip }}</span>
+                    </v-tooltip>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+            <v-row v-else>
+              <p>
+                This file format isn't supported! Please check the supported
+                file formats documentation for more information
+              </p>
+            </v-row>
+          </v-stepper-content>
+
+          <v-stepper-step
+            step="3"
+            :complete="currentStep > 3"
+            @click="SetStep(3)"
+          >
+            Inspect your file
+          </v-stepper-step>
+          <v-stepper-content step="3">
+            <v-btn color="primary" @click="InspectFile(files[0])">
+              Inspect
+            </v-btn>
+            <v-btn text @click="SetStep(2)"> Cancel </v-btn>
+          </v-stepper-content>
+
+          <v-stepper-step step="4"> Inspection results </v-stepper-step>
+          <v-stepper-content step="4">
+            <InspectorResultsPanels
+              v-if="modelChecks.length"
+              :modelChecks="modelChecks"
+              :object="GeodeObject"
+              :filename="this.files[0].name"
+              class="pa-2"
+            />
+          </v-stepper-content>
+        </v-stepper>
+      </v-col>
+      <v-col v-if="cloudRunning">
+        <PackagesVersions :versions="versions" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import CloudLoading from '@/components/CloudLoading.vue'
+import InspectorResultsPanels from '@/components/InspectorResultsPanels.vue'
+import PackagesVersions from '@/components/PackagesVersions.vue'
+import geode_objects from '@/assets/geode_objects'
+
 export default {
   name: 'ValidityChecker',
+  components: { CloudLoading, InspectorResultsPanels, PackagesVersions },
   head() {
     return {
       title: 'Validity checker',
     }
+  },
+  data() {
+    return {
+      acceptedExtensions: '',
+      extension: '',
+      currentStep: 1,
+      fileExtensions: [],
+      files: [],
+      GeodeObject: '',
+      GeodeObjects: geode_objects,
+      inputMessage: 'Please select a file',
+      inputRules: [(value) => !!value || 'The file is mandatory'],
+      items: [
+        {
+          icon: 'mdi-file-check',
+          title: 'Supported file formats',
+          href: 'https://docs.geode-solutions.com/formats/',
+        },
+        {
+          icon: 'mdi-github',
+          title: 'OpenGeode-Inspector GitHub repo',
+          href: 'https://github.com/Geode-solutions/OpenGeode-Inspector',
+        },
+      ],
+      modelChecks: [],
+      multiple: false,
+      objects: [],
+      ResultBoolean: [],
+      success: false,
+      versions: [],
+    }
+  },
+  computed: {
+    ...mapState(['ID', 'cloudRunning']),
+  },
+  created() {
+    for (var i = 0; i < this.modelChecks.length; i++) {
+      this.ResultBoolean.push(null)
+    }
+  },
+  watch: {
+    cloudRunning(newValue) {
+      if (newValue === true) {
+        this.GetAllowedFiles()
+        this.GetPackagesVersions()
+      }
+    },
+  },
+  activated() {
+    if (this.cloudRunning === true) {
+      this.GetAllowedFiles()
+      this.GetPackagesVersions()
+    }
+  },
+  methods: {
+    async GetAllowedFiles() {
+      const data = await this.$axios.$get(
+        `${this.ID}/validitychecker/allowedfiles`
+      )
+      const extensions = data.extensions.map((extension) => '.' + extension)
+      this.acceptedExtensions = extensions.join(',')
+    },
+    async GetPackagesVersions() {
+      const data = await this.$axios.$get(`${this.ID}/validitychecker/versions`)
+      this.versions = data.versions
+    },
+
+    async GetAllowedObjects(changedFiles) {
+      this.success = true
+      this.message = 'File(s) selected'
+      if (this.multiple) {
+        this.files = changedFiles
+      } else {
+        this.files = [changedFiles]
+      }
+
+      const params = new FormData()
+      params.append('filename', this.files[0].name)
+
+      const data = await this.$axios.$post(
+        `${this.ID}/validitychecker/allowedobjects`,
+        params
+      )
+      this.objects = data.objects
+      this.currentStep = this.currentStep + 1
+    },
+    SetGeodeObject(object) {
+      this.GeodeObject = object
+      this.currentStep = this.currentStep + 1
+    },
+
+    SetStep(step) {
+      if (step <= 3) {
+        this.modelChecks = []
+      }
+      if (step <= 2) {
+        this.GeodeObject = ''
+      }
+      if (step <= 1) {
+        this.files = []
+      }
+      this.currentStep = step
+    },
+
+    async InspectFile() {
+      await this.UploadFile()
+      this.SetStep(4)
+      await this.GetTestsNames()
+    },
+    async GetTestsNames() {
+      const self = this
+      const params = new FormData()
+      params.append('object', self.GeodeObject)
+      await self.$axios
+        .post(`${self.ID}/validitychecker/testsnames`, params)
+        .then((response) => {
+          if (response.status == 200) {
+            self.modelChecks = response.data.modelChecks
+            for (var i = 0; i < this.modelChecks.length; i++) {
+              this.ResultBoolean.push(null)
+            }
+          }
+        })
+    },
+    async UploadFile() {
+      const self = this
+      const reader = new FileReader()
+      reader.onload = async function (event) {
+        const params = new FormData()
+        params.append('file', event.target.result)
+        params.append('filename', self.files[0].name)
+        await self.$axios
+          .post(`${self.ID}/validitychecker/uploadfile`, params)
+          .then((response) => {
+            if (response.status == 200) {
+              console.log(response)
+            }
+          })
+      }
+      await reader.readAsDataURL(this.files[0])
+    },
   },
 }
 </script>
