653f5e1f491dd66e23c3ebaa845a2cd55eca6cc9
Arnaud Botella <botella.arnaud@gmail.com>
2022-06-16 23:19:32 +0200

    test

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -1,24 +1,6 @@
 const axios = require('axios')
 
-exports.handler = async function(event) {
-    if (event.httpMethod !== 'POST') {
-        return { 
-            statusCode: 405 
-        }
-    }
-    if (!RECAPTCHA_SECRET_KEY in process.env) {
-        console.error('RECAPTCHA_SECRET_KEY environment variable is not set.')
-        return {
-            statusCode: 500
-        }
-    }
-    if (!'token' in event.queryStringParameters) {
-        console.error('Client response token not found in URL query string.')
-        return {
-            statusCode: 400
-        }
-    }
-    
+exports.handler = async function(event) {    
         const headers = {
           'Access-Control-Allow-Origin': 'https://next.geode-solutions.com'
           // 'Access-Control-Allow-Methods': 'POST, OPTIONS'
