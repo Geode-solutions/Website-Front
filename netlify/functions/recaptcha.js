e52edf3f8d968df339a6aab0a4cd028e7def01c3
Arnaud Botella <botella.arnaud@gmail.com>
2022-06-16 23:27:16 +0200

    test

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -1,10 +1,30 @@
 const axios = require('axios')
 
-exports.handler = async function(event) {    
-        const headers = {
-          'Access-Control-Allow-Origin': 'https://next.geode-solutions.com'
-          // 'Access-Control-Allow-Methods': 'POST, OPTIONS'
-        }
-    const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${event.queryStringParameters.token}`, {headers})
-    return response
+exports.handler = async function (event) {
+  try {
+    const headers = {
+      'Access-Control-Allow-Origin': 'https://next.geode-solutions.com'
+      // 'Access-Control-Allow-Methods': 'POST, OPTIONS'
+    }
+    const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${event.queryStringParameters.token}`, { headers })
+    console.log(response)
+    if (response.success) {
+      return {
+        success: true,
+        message: 'Token verifyed',
+        response: response
+      }
+    } 
+    return {
+      success: false,
+      message: 'Invalid token',
+      response: response
+    }
+  } catch (e) {
+    console.log('ReCaptcha error:', e)
+    return {
+      success: false,
+      message: 'Internal error'
+    }
+  }
 }
