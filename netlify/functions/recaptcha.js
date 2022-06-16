31beca08807507bc3b6435a2f26ae868112f8b84
Arnaud Botella <botella.arnaud@gmail.com>
2022-06-16 22:40:53 +0200

    test

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 exports.handler = async function(event) {
-    if (event.httpMethod !== 'GET') {
+    if (event.httpMethod !== 'POST') {
         return { 
             statusCode: 405 
         }
@@ -18,6 +18,11 @@ exports.handler = async function(event) {
             statusCode: 400
         }
     }
-    const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env[RECAPTCHA_SECRET_KEY]}&response=${event.queryStringParameters.token}`)
+    
+        const headers = {
+          'Access-Control-Allow-Origin': 'https://next.geode-solutions.com'
+          // 'Access-Control-Allow-Methods': 'POST, OPTIONS'
+        }
+    const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env[RECAPTCHA_SECRET_KEY]}&response=${event.queryStringParameters.token}`, {headers})
     return response
 }
--- pages/tools.vue
+++ pages/tools.vue
@@ -32,7 +32,9 @@
         <v-row class="rows" align-content="center" align="center">
           <v-col class="align" cols="12" align-self="center">
             <recaptcha class="align-center" />
-            <v-btn color="primary" @click="onSubmit()"> Submit </v-btn>
+            <v-btn color="primary" @click="onSubmit()">
+              Submit
+            </v-btn>
           </v-col>
         </v-row>
       </v-container>
