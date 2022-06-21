46ec3d925412cc3b5e269c4fdf798e698865c6ba
JulienChampagnol <julien.champagnol@geode-solutions.com>
2022-06-21 12:03:08 +0200

    feat(wip) : badge okay

--- components/Badge.vue
+++ components/Badge.vue
@@ -1,109 +1,45 @@
 <template>
-    <v-progress-circular
-        v-if="value == null"
-        size="20"
-        color="primary"
-        indeterminate
-    ></v-progress-circular>
-    <v-icon v-else-if="value == 'error'" color="error"> mdi-alert-circle-outline </v-icon>
-    <v-icon
-        v-else-if="value == expected_value"
-        color="primary"
-    >
-        mdi-check-circle-outline
-    </v-icon>
+  <v-tooltip bottom>
+    <template #activator="{ on }">
+      <v-progress-circular
+          v-if="value == null"
+          size="20"
+          color="primary"
+          indeterminate
+          v-on="on"
+      ></v-progress-circular>
+      <v-icon
+        v-else-if="value == 'error'"
+        v-on="on"
+        color="error"
+      >
+        mdi-alert-circle-outline
+      </v-icon>
+      <v-icon
+          v-else-if="value == expected_value"
+          v-on="on"
+          color="primary"
+      >
+          mdi-check-circle-outline
+      </v-icon>
+    </template>
+    <span v-if="value == null">This check is in progress</span>
+    <span v-else-if="value == 'error'">This check returned an internal error</span>
+    <span v-else-if="value == expected_value">This check has passed</span>
+    <span v-else-if="value != expected_value">This check has failed</span>
+  </v-tooltip>
 </template>
 
-<!-- 
-<template>
-  <v-container>
-    <v-expansion-panels multiple focusable v-model="display">
-      <v-expansion-panel
-        v-for="(modelCheck, index) in modelChecks"
-        :key="index"
-        class="card"
-      >
-        <v-expansion-panel-header>
-          <div>
-            <div v-if="modelCheck.value == null">
-              <v-tooltip right>
-                <template #activator="{ on }">
-                <v-progress-circular
-                  
-                  size="20"
-                  color="primary"
-                  indeterminate
-                  v-on="on"
-                ></v-progress-circular>
-                </template>
-                <span>This check is in progress</span>
-              </v-tooltip>
-            </div>
-            <div v-else-if="modelCheck.value == modelCheck.expected_value">
-              <v-tooltip right>
-                <template #activator="{ on }">
-              <v-icon
-                color="primary"
-                v-on="on"
-              >
-                mdi-check-circle-outline
-              </v-icon>
-              </template>
-              <span>This check has passed</span>
-              </v-tooltip>
-            </div>
-            <div v-else-if="modelCheck.value == 'error'"><v-tooltip right>
-                <template #activator="{ on }">
-              <v-icon
-                color="error"
-                v-on="on"
-              >
-              mdi-alert
-              </v-icon>
-              </template>
-              <span>This check returned an internal error</span>
-              </v-tooltip>
-            </div>
-            <div
-                v-else color="error"
-              >
-            <v-icon > mdi-close-circle-outline </v-icon>
-            </div>
-            {{ modelCheck.validity_sentence }}
-          </div>
-        </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <InspectorResultsPanels
-            v-if="!modelCheck.is_leaf"
-            :index="index"
-            :modelChecks="modelCheck.list_invalidity"
-            :object="object"
-            :filename="filename"
-            @updateResult="updateResult"
-          />
-          <v-container v-else-if="modelCheck.value!=null" class="pt-6">
-            Result = {{ modelCheck.value }}
-          </v-container>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-  </v-container>
-</template> -->
-
 <script>
-
 export default {
   name: 'Badge',
   props: {
     value: {
-      type: Array,
       required: true,
     },
     expected_value: {
-      type: String,
       required: true,
     },
   }
 }
 </script>
-
--- components/InspectorResultsPanels.vue
+++ components/InspectorResultsPanels.vue
@@ -8,7 +8,7 @@
       >
         <v-expansion-panel-header>
           <div>
-            <Badge :value="toto"/>
+            <Badge :value="modelCheck.value" :expected_value="modelCheck.expected_value"/>
             {{ modelCheck.validity_sentence }}
           </div>
         </v-expansion-panel-header>
@@ -29,82 +29,6 @@
     </v-expansion-panels>
   </v-container>
 </template>
-
-<!-- 
-<template>
-  <v-container>
-    <v-expansion-panels multiple focusable v-model="display">
-      <v-expansion-panel
-        v-for="(modelCheck, index) in modelChecks"
-        :key="index"
-        class="card"
-      >
-        <v-expansion-panel-header>
-          <div>
-            <div v-if="modelCheck.value == null">
-              <v-tooltip right>
-                <template #activator="{ on }">
-                <v-progress-circular
-                  
-                  size="20"
-                  color="primary"
-                  indeterminate
-                  v-on="on"
-                ></v-progress-circular>
-                </template>
-                <span>This check is in progress</span>
-              </v-tooltip>
-            </div>
-            <div v-else-if="modelCheck.value == modelCheck.expected_value">
-              <v-tooltip right>
-                <template #activator="{ on }">
-              <v-icon
-                color="primary"
-                v-on="on"
-              >
-                mdi-check-circle-outline
-              </v-icon>
-              </template>
-              <span>This check has passed</span>
-              </v-tooltip>
-            </div>
-            <div v-else-if="modelCheck.value == 'error'"><v-tooltip right>
-                <template #activator="{ on }">
-              <v-icon
-                color="error"
-                v-on="on"
-              >
-              mdi-alert
-              </v-icon>
-              </template>
-              <span>This check returned an internal error</span>
-              </v-tooltip>
-            </div>
-            <div
-                v-else color="error"
-              >
-            <v-icon > mdi-close-circle-outline </v-icon>
-            </div>
-            {{ modelCheck.validity_sentence }}
-          </div>
-        </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <InspectorResultsPanels
-            v-if="!modelCheck.is_leaf"
-            :index="index"
-            :modelChecks="modelCheck.list_invalidity"
-            :object="object"
-            :filename="filename"
-            @updateResult="updateResult"
-          />
-          <v-container v-else-if="modelCheck.value!=null" class="pt-6">
-            Result = {{ modelCheck.value }}
-          </v-container>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-  </v-container>
-</template> -->
 
 <script>
 import { mapState } from 'vuex'
