c625b5135eafad109cea1d5159a8942a6c3411c3
JulienChampagnol <julien.champagnol@geode-solutions.com>
2022-12-20 14:50:30 +0100

    ⬆️ migrate to vue3/nuxt3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
-module.exports = {
+export default = {
   root: true,
-  env: {node: true},
+  env: { node: true },
   extends: ['eslint:recommended', 'plugin:vue/recommended'],
   plugins: ['vue', 'vuetify'],
   rules: {
     'no-console': 'off',
     'no-debugger': 'off',
     'vue/return-in-computed-property':
-        ['error', {treatUndefinedAsUnspecified: false}],
+      ['error', { treatUndefinedAsUnspecified: false }],
     'vuetify/no-deprecated-classes': 'error',
     'vuetify/grid-unknown-attributes': 'error',
     'vuetify/no-legacy-grid': 'error',
     'semi': [2, 'never'],
     'vue/max-attributes-per-line': 'off'
   },
-  parserOptions: {parser: 'babel-eslint'}
+  parserOptions: { parser: 'babel-eslint' }
 }
---
+++ nuxt3/geode-solutions.com/README.md
@@ -0,0 +1,24 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/df0a4b86-e5ec-497d-96b9-07e67d56fb95/deploy-status)](https://app.netlify.com/sites/geode-solutions/deploys)
+
+# geode-solutions.com
+
+> Website for Geode-solutions
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
