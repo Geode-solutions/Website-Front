4c1e8779b871c895b2799887c412d44b428dffd2
Arnaud Botella <arnaud.botella@geode-solutions.com>
2023-02-05 15:25:53 +0100

    add particules

--- pages/opengeode.vue
+++ pages/opengeode.vue
@@ -1,31 +1,82 @@
 <template>
   <v-row>
-    <v-col
-      class="text-center text-h6"
-      cols="12"
-    >
+    <v-col class="text-center text-h4 title" cols="12">
       <h1>
         OpenGeode
       </h1>
     </v-col>
-    <!-- 
     <client-only>
-      <Particles
-        class="particles-wrapper mb-5"
-        style="z-index: 1"
-        color="004D40"
-        :particle-opacity="0.5"
-        :particles-number="80"
-        shape-type="circle"
-        :particle-size="5"
-        lines-color="00897B"
-        :lines-width="1"
-        :line-linked="true"
-        :line-opacity="0.8"
-        :lines-distance="150"
-        :move-speed="4"
-      />
-    </client-only> -->
+      <Particles id="tsparticles" class="particles-wrapper mb-5" style="z-index: 1" :particlesInit="particlesInit"
+        :options="{
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push'
+              },
+              resize: true
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40
+              },
+              push: {
+                quantity: 2
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4
+              }
+            }
+          },
+          fullScreen: {
+            enable: false
+          },
+          particles: {
+            color: {
+              value: '#004D40'
+            },
+            links: {
+              color: '#00897B',
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1
+            },
+            collisions: {
+              enable: true
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outMode: 'bounce',
+              random: false,
+              speed: 3,
+              straight: false
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800
+              },
+              value: 80
+            },
+            opacity: {
+              value: 0.5
+            },
+            shape: {
+              type: 'circle'
+            },
+            size: {
+              random: true,
+              value: 5
+            }
+          }
+        }" />
+    </client-only>
 
     <v-container>
       <v-row class="text-center">
@@ -36,70 +87,36 @@
           </h2>
 
           <v-hover v-slot="{ hover }">
-            <v-card
-              class="mx-auto"
-              max-width="500"
-              rounded
-              :elevation="hover ? 20 : 2"
-              href="https://github.com/Geode-solutions/OpenGeode"
-              target="_blank"
-            >
-              <v-card-title
-                primary-title
-                class="justify-center text-h3"
-              >
+            <v-card class="mx-auto" max-width="500" rounded :elevation="hover ? 20 : 2"
+              href="https://github.com/Geode-solutions/OpenGeode" target="_blank">
+              <v-card-title primary-title class="justify-center text-h3">
                 Visit GitHub repo
               </v-card-title>
               <v-card-text>
-                <v-icon
-                  :size="smAndUp ? 180 : 100"
-                  color="#000000"
-                >
+                <v-icon :size="smAndUp ? 180 : 100" color="#000000">
                   mdi-github
                 </v-icon>
               </v-card-text>
             </v-card>
           </v-hover>
 
-          <h2
-            class="section text-h2 mt-5"
-            align="center"
-          >
+          <h2 class="section text-h2 mt-5" align="center">
             Key features
           </h2>
           <v-row justify="space-around">
-            <v-col
-              v-for="card in cards_list"
-              :key="card.title"
-              cols="11"
-              sm="5"
-              md="6"
-            >
+            <v-col v-for="card in cards_list" :key="card.title" cols="11" sm="5" md="6">
               <v-card height="100%">
-                <v-img
-                  :src="card.image"
-                  :alt="'Geode-solutions ' + card.title"
-                  cover
-                />
-                <v-card-title
-                  primary-title
-                  class="justify-center text-h4"
-                >
+                <v-img :src="card.image" :alt="'Geode-solutions ' + card.title" cover />
+                <v-card-title primary-title class="justify-center text-h4">
                   {{ card.title }}
                 </v-card-title>
-                <v-card-text
-                  align="justify"
-                  class="text-body-1"
-                >
+                <v-card-text align="justify" class="text-body-1">
                   {{ card.text }}
                 </v-card-text>
               </v-card>
             </v-col>
           </v-row>
-          <h2
-            class="text-h4 my-5 py-5"
-            align="center"
-          >
+          <h2 class="text-h4 my-5 py-5" align="center">
             Interested by OpenGeode features?
             <div class="my-2">
               <CommonContactUsButton />
@@ -108,16 +125,10 @@
         </v-col>
       </v-row>
     </v-container>
-    <v-container
-      fluid
-      style="background-color: white"
-    >
+    <v-container fluid style="background-color: white">
       <v-row class="container mx-auto">
         <v-col>
-          <h2
-            class="section text-h2"
-            align="center"
-          >
+          <h2 class="section text-h2" align="center">
             They use it
           </h2>
           <CommonCarousel :items="users_list" />
@@ -128,7 +139,6 @@
 </template>
 
 <script setup>
-import Particles from "vue3-particles";
 import { useDisplay } from 'vuetify'
 import road from '@/assets/img/opengeode/road.jpg'
 import canyon from '@/assets/img/opengeode/antelope-canyon.jpg'
@@ -140,6 +150,12 @@ import logo_totalenergies from '@/assets/img/logos/logo_totalenergies.png'
 import logo_ring from '@/assets/img/logos/logo_ring.png'
 import logo_gsc from '@/assets/img/logos/logo_GSC.png'
 import logo_bsc from '@/assets/img/logos/logo_BSC.png'
+
+import { loadFull } from "tsparticles";
+
+const particlesInit = async engine => {
+  await loadFull(engine);
+};
 
 useHead({ titleTemplate: 'OpenGeode - open source framework for geometric models' })
 
