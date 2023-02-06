0145bdc2e83b4a2b3ebdd94c57e673f555426d51
Arnaud Botella <arnaud.botella@geode-solutions.com>
2023-02-06 09:51:13 +0100

    add menu (#82)

--- components/Common/Header.vue
+++ components/Common/Header.vue
@@ -1,14 +1,28 @@
 <template>
-  <v-app-bar color="primary" extension-height="100px" floating>
+  <v-app-bar color="primary" floating>
     <v-row class="px-2">
-      <v-btn variant="text" to="/" :active="false">
-        <GeodeLogo />
-        {{ company_name }}
-      </v-btn>
+      <v-col>
+        <v-btn variant="text" to="/" :active="false">
+          <GeodeLogo />
+          {{ company_name }}
+        </v-btn>
+      </v-col>
       <v-spacer />
-      <v-btn v-for="(menu, i) in menus" :key="i" variant="text" :to="menu.to">
-        {{ menu.title }}
-      </v-btn>
+      <v-col cols="auto">
+        <v-btn class="hidden-sm-and-down" v-for="(menu, i) in menus" :key="i" variant="text" :to="menu.to">
+          {{ menu.title }}
+        </v-btn>
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-btn class="hidden-md-and-up" icon="mdi-menu" v-bind="props"></v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(menu, i) in menus" :key="i" :value="index" :to="menu.to">
+              <v-list-item-title>{{ menu.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-col>
     </v-row>
   </v-app-bar>
 </template>
