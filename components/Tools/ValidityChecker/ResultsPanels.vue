4c1276255134a705dcfd5ae8c701e1b2a4879c0f
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-02-21 15:08:54 +0100

    ♻️ remove dead code

--- components/Tools/ValidityChecker/ResultsPanels.vue
+++ components/Tools/ValidityChecker/ResultsPanels.vue
@@ -17,7 +17,7 @@
             input_model_checks: check.children,
             input_geode_object: input_geode_object,
             input_file_name: input_file_name
-          }" :input_index_array="temp_input_file_name.concat[index]" />
+          }" :index="index" :input_index_array="update_array(index)" />
           <v-container v-else-if="check.value == false">
             Invalid = {{ check.list_invalidities }}
           </v-container>
@@ -32,7 +32,7 @@ const stepper_tree = inject('stepper_tree')
 
 const props = defineProps({
   component_options: { type: Object, required: true },
-  input_index_array: { type: Array, required: false, default: [0] },
+  input_index_array: { type: Array, required: false, default: [] },
 })
 
 const {
@@ -40,32 +40,30 @@ const {
   input_geode_object,
   input_file_name } = props.component_options
 
-console.log('input_model_checks', input_model_checks)
-
 const { input_index_array } = props
-const temp_input_index_array = input_index_array
-console.log('temp_input_index_array', temp_input_index_array)
+
 const { tool_route } = stepper_tree
 const opened_panels = ref([])
 
 watch(input_model_checks, () => {
-  let nb_results = 0
+  let nb_results = 0;
   for (let index = 0; index < input_model_checks.length; index++) {
     const current_check = input_model_checks[index]
     if (current_check.value == null) {
       continue
     }
     if (current_check.value != true) {
+      console.log("watcher false", index)
       update_result(stepper_tree.model_checks, input_index_array, false)
       return
-    } else if (current_check.value == true) {
-      let index_of_index = opened_panels.value.indexOf(index)
-      if (index_of_index > -1) { // only splice array when item is found
-        opened_panels.value.splice(index_of_index, 1)
-      }
+    }
+    let index_of_index = opened_panels.value.indexOf(index)
+    if (index_of_index > -1) {
+      opened_panels.value.splice(index_of_index, 1)
     }
     nb_results++
   }
+  console.log("watcher true")
   if (nb_results == input_model_checks.length) {
     update_result(stepper_tree.model_checks, input_index_array, true)
   }
@@ -77,18 +75,23 @@ onMounted(() => {
   opened_panels.value = Array.from(Array(input_model_checks.length).keys())
 })
 
-function update_result (model_checks, input_index_array, value, list_invalidities = []) {
-  const index = input_index_array[0]
-  // console.log(index)
-  model_checks[index].value = value
-  if (model_checks[index].is_leaf) {
-    model_checks[index].list_invalidities = list_invalidities
-  }
-  input_index_array.shift()
-  console.log(input_index_array)
-  if (input_index_array.length) {
+function update_array (index) {
+  const array = input_index_array.concat(index)
+  console.log("array", array, index)
+  return array
+}
 
-    update_result(model_checks.children, input_index_array, value, list_invalidities)
+function update_result (model_checks, index_array, value, list_invalidities = []) {
+  if (!index_array.length) { return }
+  if (index_array.length == 1) {
+    const index = index_array[0]
+    model_checks[index].value = value
+    if (model_checks[index].is_leaf) {
+      model_checks[index].list_invalidities = list_invalidities
+    }
+  } else {
+    const new_index_array = index_array.slice(1)
+    update_result(model_checks[index_array[0]].children, new_index_array, value, list_invalidities)
   }
 }
 
@@ -96,24 +99,22 @@ async function get_tests_results () {
   for (let index = 0; index < input_model_checks.length; index++) {
     const check = input_model_checks[index]
     if (check.is_leaf && check.value == undefined) {
-      console.log('là', index, input_model_checks.length)
-      console.log('input_index_array', input_index_array)
       const params = new FormData()
       params.append('object', input_geode_object)
       params.append('filename', input_file_name)
       params.append('test', check.route)
-      console.log('cici')
-      const { data, error } = await api_fetch(`${tool_route}/inspectfile`, {
+
+      const children_array = input_index_array.concat(index)
+      const { data, error } = api_fetch(`${tool_route}/inspectfile`, {
         body: params, method: 'POST',
+
         onResponse ({ request, response, options }) {
-          // Process the response data
-          console.log('response', response)
-          console.log('input_index_array', input_index_array)
-          update_result(stepper_tree.model_checks, input_index_array, data.value.Result, data.value.list_invalidities)
-        },
-        onResponseError ({ request, response, options }) {
-          // Handle the response errors
-          // update_result (stepper_tree.model_checks, input_index_array, 'error')
+          if (response.status == 200) {
+            update_result(stepper_tree.model_checks, children_array, response._data.Result, response._data.list_invalidities)
+          }
+          else {
+            update_result(stepper_tree.model_checks, children_array, 'error')
+          }
         }
       })
     }
