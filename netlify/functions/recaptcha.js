a01e06ccc4403df730c3a1c067a02453d6165107
Clemamolette <clementinegaga79@outlook.fr>
2023-08-17 09:09:08 +0200

    Test recaptcha

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -1,4 +1,4 @@
-exports.handler = async function (event) {
+export async function handler (event) {
   try {
     const response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${event.queryStringParameters.token}`, { method: 'POST' })
       .then(response => response.data)
