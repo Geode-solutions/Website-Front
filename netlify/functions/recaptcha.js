e40cc701e331184de320c8d13117c6e8a54792c3
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-08-17 10:20:10 +0200

    test import fetch

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -1,27 +1,20 @@
+import fetch from 'node-fetch';
+
 exports.handler = async function (event) {
   try {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        console.log(xhr.status);
-        console.log(xhr.responseText);
-        return {
-          statusCode: 400,
-          body: JSON.stringify(response)
-        }
+    const response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${event.queryStringParameters.token}`, { method: 'POST' })
+      .then(response => response.data)
+    console.log(response)
+    if (response.success) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response)
       }
-      if (this.status == 200) {
-        var data = JSON.parse(this.responseText);
-        return {
-          statusCode: 200,
-          body: JSON.stringify(response)
-        }
-      }
-    };
-
-
-    xhr.open("POST", `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${event.queryStringParameters.token}`);
-    xhr.send();
+    }
+    return {
+      statusCode: 400,
+      body: JSON.stringify(response)
+    }
   } catch (e) {
     console.log('ReCaptcha error:', e)
     return {
