2000ed764ada1d36f0c4f20a3e449ca5bf8ce960
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-08-17 10:28:33 +0200

    test

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -3,20 +3,15 @@ import fetch from 'node-fetch';
 exports.handler = async function (event) {
   try {
     const response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${event.queryStringParameters.token}`, { method: 'POST' })
-      .then(response => response.data)
-    console.log(response)
-    if (response.success) {
+
+    if (response.ok) {
       return {
         statusCode: 200,
         body: JSON.stringify(response)
       }
     }
-    return {
-      statusCode: 400,
-      body: JSON.stringify(response)
-    }
-  } catch (e) {
-    console.log('ReCaptcha error:', e)
+  } catch (error) {
+    console.log(error);
     return {
       statusCode: 500,
       body: 'Internal error'
