6f8973e641ffc96d450720da9eb5668bf5376a75
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-09-27 17:17:01 +0200

    fix(site_key): remove site_key from pages

--- pages/tools/validity_checker.vue
+++ pages/tools/validity_checker.vue
@@ -1,5 +1,5 @@
 <template>
-  <Wrapper :site_key="site_key" :cards_list="cards_list" :stepper_tree="stepper_tree" />
+  <Wrapper :cards_list="cards_list" :stepper_tree="stepper_tree" />
 </template>
 
 <script setup>
@@ -22,7 +22,6 @@ const cards_list = [
     href: 'https://github.com/Geode-solutions/OpenGeode-Inspector',
   },
 ]
-const site_key = useRuntimeConfig().public.SITE_KEY
 
 const files = ref([])
 const geode_object = ref('')
