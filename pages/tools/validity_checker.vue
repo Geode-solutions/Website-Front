2020cb6219b5197754fe2cced4bcacfbbea89759
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-12-06 16:00:22 +0100

    migrate to integrate changes

--- pages/tools/validity_checker.vue
+++ pages/tools/validity_checker.vue
@@ -63,7 +63,9 @@
         component: {
           component_name: shallowRef(ObjectSelector),
           component_options: {
-            files: files,
+            filenames: computed(() => {
+              return files.value.map((file) => file.name)
+            }),
             key: key,
           },
         },
@@ -82,7 +84,9 @@
           component_options: {
             multiple: true,
             input_geode_object: input_geode_object,
-            files: files,
+            filenames: computed(() => {
+              return files.value.map((file) => file.name)
+            }),
             route: "tools/upload_file",
           },
         },
