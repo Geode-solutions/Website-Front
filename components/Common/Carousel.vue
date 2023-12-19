816e2f8f6d269032869bb0d1cb8bce7c63f64803
yomoxw <stagegeode@gmail.com>
2023-12-19 14:07:03 +0100

    first commit: Tom carousel

--- components/Common/Carousel.vue
+++ components/Common/Carousel.vue
@@ -1,6 +1,6 @@
 <template>
-  <ClientOnly>
-    <Carousel :settings="carrousel_settings">
+  <ClientOnly> 
+     <Carousel :settings="carrousel_settings"> 
       <Slide
         v-for="(item, index) in props.items"
         :key="index"
@@ -9,40 +9,40 @@
         <a :href="item.url" target="_blank">
           <img :src="item.logo" class="carousel__item" />
         </a>
-      </Slide>
+      </Slide> 
       <template #addons>
-        <Navigation />
-        <Pagination />
-      </template>
-    </Carousel>
+        <Navigation />  
+        <Pagination /> 
+     </template> 
+    </Carousel> 
   </ClientOnly>
 </template>
 
-<script setup>
+<script setup> 
   import "vue3-carousel/dist/carousel.css"
   import { Carousel, Slide, Navigation, Pagination } from "vue3-carousel"
   import { useDisplay } from "vuetify"
 
   const props = defineProps({
-    items: { type: Array, required: true },
+   items: { type: Array, required: true },
   })
 
-  const { name } = toRefs(useDisplay())
-  const nb_items_to_display = ref(3)
-  watch(
+   const { name } = toRefs(useDisplay())
+   const nb_items_to_display = ref(3)
+watch(
     name,
     (value) => {
-      switch (value) {
-        case "xs": {
-          nb_items_to_display.value = 1
+       switch (value) {
+         case "xs": {
+           nb_items_to_display.value = 1
           break
-        }
+       }
         case "sm": {
           nb_items_to_display.value = 2
           break
         }
         default: {
-          nb_items_to_display.value = 3
+         nb_items_to_display.value = 3
           break
         }
       }
@@ -50,7 +50,7 @@
     {
       immediate: true,
     },
-  )
+   )  
 
   const carrousel_settings = reactive({
     autoplay: 2000,
@@ -59,7 +59,7 @@
     pauseAutoplayOnHover: true,
     transition: 1000,
     wrapAround: true,
-  })
+})
 
   onMounted(() => {
     console.log(carrousel_settings)
@@ -73,22 +73,21 @@
     background-color: var(--vc-clr-white);
     color: var(--vc-clr-white);
     font-size: 20px;
-    border-radius: 8px;
+    border-radius: 10px;
     display: flex;
     justify-content: center;
     align-items: center;
-  }
-
-  .carousel__slide {
+  } 
+  .carousel__slide { 
     display: flex;
-    padding: 10px;
+    padding: 15px;
     align-items: center;
     justify-content: center;
-  }
+  } 
 
-  .carousel__prev,
+  .carousel__prev, 
   .carousel__next {
     box-sizing: content-box;
-    border: 5px solid white;
-  }
+    border: 5px solid red; 
+  } 
 </style>
